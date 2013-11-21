039d611f17b556eb8a787c6da36a0bb3830b74af
Alyssa Sallean <alyssa.sallean@yahoo.com>
2013-11-21 00:19:09 -0600

    I hate you git

--- light.cpp
+++ light.cpp
@@ -15,7 +15,12 @@
 #include "common.h"
 #include "raytrace.h"
 
-material* makeMaterial(GLfloat r, GLfloat g, GLfloat b, GLfloat amb) {
+GLfloat dotProduct(vector* v1, vector* v2);
+void ambientReflection(color *c , GLfloat ambientCoef, material* m);
+void specularReflection(vector* outgoingSpecularIntensity, GLfloat specularCoef, vector* reflectionVector, vector* viewVector, vector* incomingSpecularIntensity);
+void diffuseReflection(vector* outgoingDiffuseIntensity, GLfloat diffuseCoef, vector* normalVector, vector* lightVector, vector* incomingDiffuseIntensity);
+
+material* makeMaterial(GLfloat r, GLfloat g, GLfloat b, GLfloat amb, GLfloat dif, GLfloat spec, GLfloat s) {
   material* m;
   
   /* allocate memory */
@@ -25,6 +30,9 @@ material* makeMaterial(GLfloat r, GLfloat g, GLfloat b, GLfloat amb) {
   m->g = g;
   m->b = b;
   m->amb = amb;
+  m->dif = dif;
+  m->spec = spec;
+  m->s = s;
   return(m);
 }
 
@@ -44,9 +52,9 @@ light* makeLight(GLfloat x, GLfloat y ,GLfloat z, GLfloat vx, GLfloat vy, GLfloa
   l->r->dir->z = vz;
   l->r->dir->w = 0.0;
 
-  l->r = r;
-  l->g = g;
-  l->b = b;
+  l->red = r;
+  l->green = g;
+  l->blue = b;
 
   return(l);
 }
@@ -58,15 +66,75 @@ light* makeLight(GLfloat x, GLfloat y ,GLfloat z, GLfloat vx, GLfloat vy, GLfloa
 /* in is the direction of the incoming ray and d is the recusive depth */
 void shade(point* p, vector* n, material* m, vector* in, color* c, int d, light* l) {
 
-  /* so far, just finds ambient component of color */
-  c->r = m->amb * m->r;
-  c->g = m->amb * m->g;
-  c->b = m->amb * m->b;
-  
+
+  color *Ia = (color*)malloc(sizeof(color));
+  color *Id = (color*)malloc(sizeof(color));
+  color *Is = (color*)malloc(sizeof(color));  
+
+  ambientReflection(Ia, m->amb, m);
+
   /* clamp color values to 1.0 */
   if (c->r > 1.0) c->r = 1.0;
   if (c->g > 1.0) c->g = 1.0;
   if (c->b > 1.0) c->b = 1.0;
 
 }
+
+
+
+void ambientReflection(color *c , GLfloat ambientCoef, material* m){
+  c->r = ambientCoef*m->r;
+  c->g = ambientCoef*m->g;
+  c->b = ambientCoef*m->b;
+}
+
+void diffuseReflection(vector* outgoingDiffuseIntensity, GLfloat diffuseCoef, vector* normalVector, vector* lightVector, vector* incomingDiffuseIntensity) {
+  GLfloat max = 0;
+  if ( 0 < dotProduct(normalVector, lightVector) )
+    max = dotProduct(normalVector, lightVector);
+  
+  outgoingDiffuseIntensity->x = diffuseCoef *
+    max * 
+    incomingDiffuseIntensity->x;
+  
+  outgoingDiffuseIntensity->y = diffuseCoef *
+    max * 
+    incomingDiffuseIntensity->y;
+  
+  outgoingDiffuseIntensity->z = diffuseCoef *
+    max * 
+    incomingDiffuseIntensity->z;
+  
+}
+
+void specularReflection(vector* outgoingSpecularIntensity, GLfloat specularCoef, vector* reflectionVector, vector* viewVector, vector* incomingSpecularIntensity) {
+  
+  GLfloat pizza = dotProduct( reflectionVector, viewVector);
+  
+  outgoingSpecularIntensity->x = specularCoef *
+    pizza *
+    incomingSpecularIntensity->x;
+
+  outgoingSpecularIntensity->y = specularCoef *
+    pizza *
+    incomingSpecularIntensity->y;
+  
+  outgoingSpecularIntensity->z = specularCoef *
+    pizza *
+    incomingSpecularIntensity->z;
+  
+}
+
+
+GLfloat dotProduct(vector* v1, vector* v2) {
+  
+  return ( v1->x * v2->x ) + 
+    ( v1->y * v2->y ) + 
+    ( v1->z * v2->z ); 
+  
+}
+
+
+
+
 
